99c5a2c9237f9ff58ec2c619f560862c279e38bf
Projectslearner <projectslearner@gmail.com>
2024-06-10 07:26:19 +0000

    code

---
+++ led.c
@@ -0,0 +1,21 @@
+/*
+    Project name : LED
+    Modified Date: 10-06-2024
+    Code by : Projectslearner
+    Website : https://projectslearner.com/learn/arduino-nano-led
+*/
+
+const int ledPin = 13; // Pin connected to the LED
+
+void setup() 
+{
+  pinMode(ledPin, OUTPUT); // Set the LED pin as an output
+}
+
+void loop() 
+{
+  digitalWrite(ledPin, HIGH); // Turn on the LED
+  delay(1000); // Wait for 1 second (1000 milliseconds)
+  digitalWrite(ledPin, LOW); // Turn off the LED
+  delay(1000); // Wait for 1 second (1000 milliseconds)
+}
